29e1857408841338479547e540f3645b64eb1e33
pouyapanahandeh <panahandehpooya@gmail.com>
2019-03-30 16:00:21 +0100

    build main content

--- github/src/App.vue
+++ github/src/App.vue
@@ -2,16 +2,13 @@
   <v-app>
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>Hey! </span>
+        <span class="font-weight-light">I'm pooya</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
+      <v-btn href="https://github.com/pouyapanahandeh" round>
+        Github
+        <v-icon right>person_pin</v-icon>
       </v-btn>
     </v-toolbar>
 
