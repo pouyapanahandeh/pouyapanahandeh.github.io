a1c9271646e2a821d18221ed54514d652f28769b
pouyapanahandeh <panahandehpooya@gmail.com>
2019-07-28 13:29:21 +0200

    navbar includin contacting form by google

---
+++ personal-page/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-container>
+      <NavBar />       
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+import NavBar from './components/NavBar';
+
+export default {
+  name: 'App',
+  components: {
+    NavBar,
+  },
+  data () {
+      return {
+        dialog: false,
+      }
+    },
+};  
+</script>
