28e87aa9c4d085d5a8711dbf1aa9831117bf63e7
pooya <pouyapanahandeh@users.noreply.github.com>
2019-09-16 13:19:47 +0200

    Adding new version of personal website.

---
+++ precache-manifest.3237d3f8dfaf047e320763c642a18c47.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "08d52c996dcc57972fc9",
+    "url": "/js/about.01e33215.js"
+  },
+  {
+    "revision": "c71c17b94a0ef297b42d",
+    "url": "/css/app.2c14151f.css"
+  },
+  {
+    "revision": "c71c17b94a0ef297b42d",
+    "url": "/js/app.ad32429a.js"
+  },
+  {
+    "revision": "3e3103b07392e3a8ad6a",
+    "url": "/css/chunk-vendors.c8655df8.css"
+  },
+  {
+    "revision": "3e3103b07392e3a8ad6a",
+    "url": "/js/chunk-vendors.f188c167.js"
+  },
+  {
+    "revision": "b1ba498fb2bc1ae83b2a71604b2fa786",
+    "url": "/img/personalPicture.b1ba498f.jpg"
+  },
+  {
+    "revision": "d3d994246e17d5ea15215d94313da3ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
